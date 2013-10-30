c30a55f5325df9a35997a4e28987082ef5ed885a
Stefano Schiavi <stefano@bvprojects.org>
2013-10-29 20:17:23 -0700

    revision 0.1

---
+++ black_jack.rb
@@ -0,0 +1,37 @@
+puts 'Welcome to BlackJack, an awesome card game'
+puts 'Please enter your name to begin'
+
+player = gets.chomp
+dealer = 'dealer'
+
+puts "welcome #{player} let's play!"
+
+suits = [ 'H', 'D', 'C', 'S' ]
+cards = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
+
+used_cards = []
+
+card = [ suits[rand(suits.length)], cards[rand(cards.length)] ]
+
+while used_cards.include?(card.join())
+
+  card = [ suits[rand(suits.length)], cards[rand(cards.length)] ]
+  used_cards << card.join()
+  return card
+
+end
+  
+
+player_cards = []
+dealer_cards = []
+
+if player_cards = []
+    player_cards << 
+    
+  end
+
+def count_score card
+
+
+  
+end
