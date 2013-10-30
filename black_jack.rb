a62c0aed4826f6726be84b08a283aaf0852c691b
Stefano Schiavi <stefano@bvprojects.org>
2013-10-29 20:40:19 -0700

    revision 0.1

--- black_jack.rb
+++ black_jack.rb
@@ -11,14 +11,19 @@ cards = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
 
 used_cards = []
 
-card = [ suits[rand(suits.length)], cards[rand(cards.length)] ]
 
-while used_cards.include?(card.join())
+
+def generate_card suits, cards
 
   card = [ suits[rand(suits.length)], cards[rand(cards.length)] ]
-  used_cards << card.join() unless used_cards.include?(card.join())
-  return card
+  
+  if used_cards.include?(card.join())
+    generate_card suits, cards 
+  end
 
+  used_cards << card.join()
+
+  
 end
   
 
